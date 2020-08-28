7e8f751941e9e4650d8f170b641d93e685b65c80
leafybillow <tao.ye@stonybrook.edu>
2020-08-27 21:09:47 -0400

    auto adjust column width

--- src/TaResult.cc
+++ src/TaResult.cc
@@ -3,17 +3,18 @@
 ClassImp(TaResult);
 ClassImp(TaRow);
 
-TaResult::TaResult(TString filename){
-  logfile.open(filename.Data());
-  column_wid=10;
+TaResult::TaResult(TString filename_in){
+
+  filename =  filename_in;
   nColumns=0;
-  cout << __PRETTY_FUNCTION__ <<  ": opening " <<  filename << endl;
 }
 
 void TaResult::Close(){
+
   logfile.close();
 }
 void TaResult::AddHeader(vector<TString> input){
+
   Int_t ncol = input.size();
   AddLine();
   for(int icol=0;icol<ncol;icol++){
@@ -23,10 +24,12 @@ void TaResult::AddHeader(vector<TString> input){
 }
 
 void TaResult::AddFloatEntry(Double_t finput){
+
   TString format_input = Form("%.2f",finput);
   this_row.AddEntry(format_input);
 }
 void TaResult::AddChi2NDF(Double_t chi2 , Double_t ndf){
+
   TString text = Form("%.1f/%.0f",chi2,ndf);
   this_row.AddEntry(text);
 }
@@ -36,8 +39,13 @@ void TaResult::AddLine(){
     fRows.push_back(this_row);
     if(this_row.size()>nColumns)
       nColumns = this_row.size();
-    if(this_row.GetMaxWidth()>column_wid)
-      column_wid = this_row.GetMaxWidth();
+    
+     for(int icol=0;icol<this_row.size();icol++){
+       if( column_wid.begin() + icol  == column_wid.end())
+	column_wid.push_back( this_row.GetColumnWidth(icol) );
+       else if( this_row.GetColumnWidth(icol) > column_wid[icol] )
+	column_wid[icol] = this_row.GetColumnWidth(icol);
+    }
     this_row.Clear();
   }
 }
@@ -48,20 +56,24 @@ void TaResult::InsertHorizontalLine(){
 }
 
 void TaResult::Print(){
+  cout << __PRETTY_FUNCTION__ <<  ": opening " <<  filename << endl;
+  
+  logfile.open(filename.Data());
   ostream& out = logfile;
   AddLine();
   Int_t nrow = fRows.size();
   TString colum_sep = "|";
   for(int irow=0;irow<nrow;irow++){
     if(fRows[irow].size()==1 && fRows[irow].GetEntry(0)=="hline"){
-      for(int i=0;i<nColumns*(column_wid+1);i++)
-	out<< "-";
+      for(int icol=0;icol<nColumns;icol++)
+	for(int i=0;i<column_wid[icol]+1;i++)
+	  out<< "-";
       out << endl;
     }
     else{
       for(int icol=0;icol<nColumns;icol++){
-	out<< setw(column_wid) << setiosflags(ios::left)
-	       << Center(fRows[irow].GetEntry(icol));
+	out<< setw(column_wid[icol]) << setiosflags(ios::left)
+	   << Center(fRows[irow].GetEntry(icol) , column_wid[icol]);
 	out << colum_sep;
       }
       out << endl;
@@ -70,14 +82,14 @@ void TaResult::Print(){
 }
 
 
-TString TaResult::Center(TString input){
+TString TaResult::Center(TString input, Int_t col_width){
 
   Int_t string_length = input.Length();
-  Int_t npad = floor( (column_wid+1 -string_length)*0.5);
+  Int_t npad = floor( (col_width+1 -string_length)*0.5);
   TString a_single_space =" ";
   TString output;
   if(input.Contains(".")){
-    npad = column_wid-string_length-2;
+    npad = col_width-string_length-2;
   }
 
   for(int i=0;i<npad;i++)
--- src/TaResult.hh
+++ src/TaResult.hh
@@ -9,36 +9,38 @@ using namespace std;
 
 class TaRow{
 public:
-  TaRow(){ max_width=10;};
+  TaRow(){};
   virtual ~TaRow(){};
   void AddEntry(TString entry){
     fEntries.push_back(entry);
-    if(entry.Length()>max_width)
-      max_width = entry.Length();
+    if(entry.Length()>10)
+      column_width.push_back(entry.Length());
+    else
+      column_width.push_back(10);
   }
   
-  Ssiz_t GetMaxWidth(){return max_width;};
+  Ssiz_t GetColumnWidth(Int_t icol ){return column_width[icol];};
   
   void Clear(){
     fEntries.clear();
-    max_width=0;
+    column_width.clear();
   }
 
   Int_t size(){ return fEntries.size();};
   TString GetEntry(int ie) {
     if(ie>=size())
-      return "n/a";
+      return "(invalid)";
     else
       return fEntries[ie];};
 private:
   vector<TString> fEntries;
-  Int_t max_width;
+  vector<Int_t> column_width;
   ClassDef(TaRow,0);
 };
 
 class TaResult{
 public:
-  TaResult(TString filename);
+  TaResult(TString filename_in);
   virtual ~TaResult(){};
   void AddStringEntry(TString finput){ this_row.AddEntry(finput);};
   void AddFloatEntry(Double_t finput);
@@ -47,13 +49,14 @@ public:
   void InsertHorizontalLine();
   void AddLine();
   void Close();
-  TString Center(TString);
+  TString Center(TString,Int_t);
   void Print();
 private:
+  TString filename ;
   ofstream logfile;
   TaRow this_row;
   vector<TaRow> fRows;
-  Int_t column_wid;
+  vector<Int_t> column_wid;
   Int_t nColumns;
   ClassDef(TaResult,0);
 };
